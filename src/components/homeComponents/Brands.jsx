74e913137e2d42feb395fc4dc6544da606e077ae
Khandaker Sojol Ahmed <khandakervlogs@gmail.com>
2025-11-17 20:12:05 +0600

    brans section done

---
+++ src/components/homeComponents/Brands.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import amazon from "../../assets/brands/amazon.png";
+import amazonVector from "../../assets/brands/amazon_vector.png";
+import casio from "../../assets/brands/casio.png";
+import moonStar from "../../assets/brands/moonstar.png";
+import randstad from "../../assets/brands/randstad.png";
+import star from "../../assets/brands/star.png";
+import startPeople from "../../assets/brands/start_people.png";
+import Marquee from "react-fast-marquee";
+
+const brandsLogos = [
+  amazon,
+  amazonVector,
+  casio,
+  moonStar,
+  randstad,
+  star,
+  startPeople,
+];
+
+const Brands = () => {
+  return (
+    <div className="py-18 max-w-7xl mx-auto">
+      <h2 className="text-3xl text-center text-[#03464D] font-bold mb-18">
+        We've helped thousands of sales teams
+      </h2>
+      <Marquee gradient={false} speed={80} pauseOnHover={true}>
+        <div className="flex items-center gap-12">
+          {brandsLogos.map((logo, index) => (
+            <img
+              key={index}
+              src={logo}
+              alt=""
+              className="transition cursor-pointer"
+            />
+          ))}
+        </div>
+      </Marquee>
+      {/* LINE */}
+      <div className="border-b border-dashed border-gray-400 mt-18"></div>
+    </div>
+  );
+};
+
+export default Brands;
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../components/homeComponents/Banner";
 import HowItWorks from "../../components/homeComponents/HowItWorks";
+import Brands from "../../components/homeComponents/Brands";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <HowItWorks></HowItWorks>
+      <Brands></Brands>
     </div>
   );
 };
